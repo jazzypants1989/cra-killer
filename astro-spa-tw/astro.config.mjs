876f9ab7b78c729a564dcb5a6caa47e589650a87
JLarky <jlarky@gmail.com>
2022-11-28 07:26:01 -0700

    put the whole CRA into Astro

--- astro-spa-tw/astro.config.mjs
+++ astro-spa-tw/astro.config.mjs
@@ -8,5 +8,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    publicDir: "../rr6-cra/public",
+  },
   integrations: [tailwind(), react()]
 });
