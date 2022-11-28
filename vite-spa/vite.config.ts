67e7cf86eeceadf1b212c4eac8ce3f491cb89fd2
JLarky <jlarky@gmail.com>
2022-11-28 11:50:07 -0700

    run cra in vite. whaaaaat?

--- vite-spa/vite.config.ts
+++ vite-spa/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: "../rr6-cra/public",
+  server: {
+    port: 3000,
+  },
   plugins: [react()]
 })
